4d6c06546c91f421fe7e66a5180a4a6b58fe1e23
s-kiser <iamthestevie@gmail.com>
2025-03-31 04:01:23 -0500

    Move SSH scripts to dedicated directory

--- skiser-ssh/skiser-ssh-key.sh
+++ skiser-ssh/skiser-ssh-key.sh
@@ -1,6 +1,6 @@
 if [[ -z $(ls -A ~/.ssh/ -I "known_hosts" -I "*.pub" -I "config") ]]; then 
   echo -e "no ssh key";
-  ssh-keygen -t rsa 
+  # ssh-keygen -t rsa 
 else
   echo -e "ssh key exists"
 fi;
